182f9b9314e539d01b04936f1ce0e8683503a461
Gabriel Antonio <gmav1010@gmail.com>
2020-05-06 10:22:48 -0300

    Leitura dos dados

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
+	string sexo;
+	int cervejas, refrigerantes, espetinhos;
+	
+	cout << "Sexo: ";
+	cin >> sexo;
+	cout << "Quantidade de cervejas: ";
+	cin >> cervejas;
+	cout << "Quantidade de refrigerantes: ";
+	cin >> refrigerantes;
+	cout << "Quantidade de espetinhos: ";
+	cin >> espetinhos;
+	
 	
 	return 0;
 }
